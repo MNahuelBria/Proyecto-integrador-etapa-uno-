051309cf67a242093bd984fdbe4760f81467e5b8
Nahuel B <nagu.b.2012@gmail.com>
2023-08-23 18:42:59 -0300

    configurando el archivo vite js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
+import { resolve } from 'node:path'
+import { build } from 'vite'
+
 export default {
     server: {
         port: 2222
     },
     css: {
         devSourcemap: true
+    },
+    build: {
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                carrito: resolve('paginas/carrito.html'),
+                contacto: resolve('paginas/contacto.html'),
+                nosotros: resolve('paginas/nosotros.html'),
+                principal: resolve('index.html')
+            }
+        }
     }
 }
